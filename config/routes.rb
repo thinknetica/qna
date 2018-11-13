501be8f661c172eff398fefb7de00a6665d8491e
Vitaly Kurennov <vkurennov@gmail.com>
2018-11-13 16:59:02 +0300

    User can sign in (lesson 5. part 1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :questions
+
+  root to: 'questions#index'
 end
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'User can sign in', %q{
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to be able to sign in
+} do
+
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+
+  background { visit new_user_session_path }
+
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+end
